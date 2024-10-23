26d0bce58a36de480dc7edd17964dcd393d529c8
Vdovenko Sergey <39949940+DeFauste@users.noreply.github.com>
2024-10-23 14:26:17 +0300

    add: inventory system

---
+++ Assets/Scripts/Core/Inventory/IItem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Core.Inventory
+{
+    public interface IItem
+    {
+        public int Id { get; protected set; }
+        public string Name { get; protected set; }
+        public Sprite Icon { get; protected set; }
+        public Vector2 Size { get; protected set; }
+    }
+}
---
+++ Assets/Scripts/Core/Inventory/InventorySystem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Assets.Scripts.Core.Inventory
+{
+    public class InventorySystem
+    {
+        private Dictionary<IItem, int> _items = new Dictionary<IItem, int>();
+        public IReadOnlyDictionary<IItem, int> Items => new ReadOnlyDictionary<IItem, int>(_items);
+
+        public void AddItem(IItem item, int quantity)
+        {
+            if(_items.ContainsKey(item))
+            {
+                _items[item] += quantity;
+            } else
+            {
+                _items.Add(item, quantity);
+            }
+        }
+
+        public (IItem, int) GetItem(IItem item, int quantity)
+        {
+            if (item == null || quantity <= 0 || !_items.ContainsKey(item))
+            {
+                return (null, 0);
+            }
+
+            var currentQuantity = _items[item];
+            if (currentQuantity < quantity)
+            {
+                return (null, 0);
+            }
+
+            _items[item] = currentQuantity - quantity;
+
+            if (_items[item] == 0)
+            {
+                _items.Remove(item);
+            }
+
+            return (item, quantity);
+        }
+
+    }
+}
