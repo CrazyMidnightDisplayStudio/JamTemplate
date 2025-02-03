865098aad3e8219d7e48ad077a44e5655dd9d7a4
Erosrolf <Erosrolf@gmail.com>
2025-02-03 20:20:32 +0300

    поправил поворот тайлов вокруг игрока. сделал генерацию новцых тайлов спереди и удаление старых

--- Assets/Scripts/Gameplay/City/CityRoot.cs
+++ Assets/Scripts/Gameplay/City/CityRoot.cs
@@ -4,7 +4,7 @@ namespace Gameplay.City
 {
     public static class CityRoot
     {
-        public static Vector3 RootPosition => Vector3.zero;
+        public static Vector3 RootPosition => new Vector3(7.5f, 0f, 0f);
         public static Vector3 MoveDirection => Vector3.back;
 
         public static Quaternion RotateDirection => Quaternion.Euler(0f, 90f, 0f);
---
+++ Assets/Scripts/Gameplay/Utils/SpeedController.cs
@@ -0,0 +1,12 @@
+﻿namespace Gameplay.Utils
+{
+    public class SpeedController
+    {
+        public float Speed { get; set; }
+
+        public SpeedController(float speed)
+        {
+            Speed = speed;
+        }
+    }
+}
