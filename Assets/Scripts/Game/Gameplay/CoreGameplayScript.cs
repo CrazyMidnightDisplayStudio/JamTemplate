8c0d79bae1d2f95e078742483068c211b93cd64f
Erosrolf <Erosrolf@gmail.com>
2025-02-12 11:47:50 +0300

    вынес логику свидания в скрипт Date

--- Assets/Scripts/Game/Gameplay/CoreGameplayScript.cs
+++ Assets/Scripts/Game/Gameplay/CoreGameplayScript.cs
@@ -1,21 +1,43 @@
-﻿using UnityEngine;
+﻿using Game.Gameplay.Utils;
+using UnityEngine;
 
 namespace Game.Gameplay
 {
     public class CoreGameplayScript : MonoBehaviour
     {
         public Player Player;
-
-        private Coroutine _dateCoroutine;
+        private Date _date;
 
         // тут может быть логика создания игрока, при выборе имени, зодиака и фигуры
         // TODO: Player = new();
 
         // TODO: написать скрипт по потбору опонента
 
-        public async void StartDateRoutine(Opponent opponent)
+        private void Awake()
         {
-            // тут будет цикл с таймером
+            GameObject dateObject = new GameObject("Date");
+            _date = dateObject.AddComponent<Date>();
+        }
+
+        public void StartDate(Opponent opponent, float dateTime)
+        {
+            _date.StartDate(dateTime, OnDate, OnDateEnd);
+        }
+
+        public void StopDate()
+        {
+            _date.StopDate();
+        }
+
+        private void OnDate()
+        {
+            // код который исполняется во время свидания
+        }
+
+        private void OnDateEnd()
+        {
+            Debug.Log("Date finished in CoreGameplayScript");
         }
     }
 }
+
---
+++ Assets/Scripts/Game/Gameplay/Utils/Date.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System;
+
+namespace Game.Gameplay.Utils
+{
+    public class Date : MonoBehaviour
+    {
+        private float _dateDuration;
+        private float _dateTimer;
+        private bool _isActive;
+
+        private Action _onDate;
+        private Action _onEndDate;
+
+        public void StartDate(float duration, Action onDate, Action onEndDate)
+        {
+            _dateDuration = duration;
+            _dateTimer = 0f;
+            _onEndDate = onEndDate;
+            _isActive = true;
+
+            Debug.Log("Date started!");
+        }
+
+        void Update()
+        {
+            if (!_isActive) return;
+
+            _onDate?.Invoke();
+
+            _dateTimer += Time.deltaTime;
+            if (_dateTimer >= _dateDuration)
+            {
+                EndDate();
+            }
+        }
+
+        /// <summary>
+        /// Завершить свидание до истечения таймера, к примеру если шкала заполнилась раньше таймера
+        /// </summary>
+        public void StopDate()
+        {
+            EndDate();
+        }
+
+        private void EndDate()
+        {
+            if (!_isActive) return;
+
+            _isActive = false;
+            Debug.Log("Date ended!");
+
+            _onEndDate?.Invoke(); // Сообщаем о завершении
+        }
+    }
+}
